a96f33be2a8b9ad35bfe2f12db33b10a89be418a
Ermanno <e.scanagatta@gmail.com>
2020-01-14 12:59:57 +0100

    Fix proxy backend address

--- gogas-service/frontend/src/setupProxy.js
+++ gogas-service/frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     proxy(["/authenticate", "/info", "/api"], {
-      target: "http://gasmio:8081",
+      target: "http://localhost:8081",
       ws: true
     })
   );
