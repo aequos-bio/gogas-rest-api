9651a11b7dfb08752da1695b36f08027ef8a75b0
Davide Lorusso <darth.loru@gmail.com>
2020-03-03 19:17:23 +0100

    Add all order types not billed by aequos

--- gogas-persistence/src/main/resources/db/migration/V20200215_001__accounting_code_order_type.sql
+++ gogas-persistence/src/main/resources/db/migration/V20200215_001__accounting_code_order_type.sql
@@ -4,4 +4,4 @@ ALTER TABLE tipologiaOrdine
 GO
 
 UPDATE tipologiaOrdine SET fatturatoDaAequos = 1
-WHERE idOrdineAequos IS NOT NULL AND idOrdineAequos NOT IN (14, 15, 22, 24) --TODO: check ordini non fatturati da aequos
+WHERE idOrdineAequos IS NOT NULL AND idOrdineAequos NOT IN (3, 14, 15, 21, 22, 24, 25, 26);
