ffc851428719dffd8d5708cae842a0af89de79b5
ermannos <e.scanagatta@gmail.com>
2021-02-20 21:15:23 +0100

    Persistenza dello stato in localstorage

--- gogas-service/frontend/src/store/store.js
+++ gogas-service/frontend/src/store/store.js
@@ -11,18 +11,47 @@ const loggerMiddleware = createLogger();
 
 const jwt = Cookies.get('jwt-token');
 
+const defaultState = {
+  authentication: {
+    running: false,
+    jwtToken: jwt,
+    userDetails: undefined,
+  },
+  info: {},
+  accounting: {
+    currentYear: Number.parseInt(moment().format('YYYY'), 10),
+  },
+};
+
+const loadState = () => {
+  let state = defaultState;
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState !== null) {
+      state = JSON.parse(serializedState);
+    }
+  } catch (err) {
+    console.error('Error while trying to idrate the state', err);
+  }
+  return state;
+};
+
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.error(err);
+    // ignore write errors
+  }
+};
+
 export const Store = createStore(
   reducers,
-  {
-    authentication: {
-      running: false,
-      jwtToken: jwt,
-      userDetails: undefined,
-    },
-    info: {},
-    accounting: {
-      currentYear: Number.parseInt(moment().format('YYYY'), 10),
-    },
-  },
+  loadState(),
   composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
 );
+
+Store.subscribe(() => {
+  saveState(Store.getState());
+});
