98519f9f5057ea783fb39557c3eec98d74d962ea
Davide <darth.loru@gmai.com>
2022-09-28 20:22:42 +0200

    Fix telegram notification message

--- gogas-service/src/main/java/eu/aequos/gogas/notification/telegram/TelegramNotificationChannel.java
+++ gogas-service/src/main/java/eu/aequos/gogas/notification/telegram/TelegramNotificationChannel.java
@@ -22,7 +22,7 @@ public class TelegramNotificationChannel implements NotificationChannel {
     public void sendOrderNotification(Order order, OrderNotificationBuilder notificationBuilder, Set<String> targetUserIds) {
         TelegramNotificationRequestDTO requestDTO = TelegramNotificationRequestDTO.builder()
                 .userIds(targetUserIds)
-                .text(notificationBuilder.getPushMessage(order))
+                .text(notificationBuilder.getTelegramMessage(order))
                 .build();
 
         String tenantId = TenantContext.getTenantId()
