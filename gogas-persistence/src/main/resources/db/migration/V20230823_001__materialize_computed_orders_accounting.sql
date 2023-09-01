14fbfcfe942a11bb95a5bd39e245e1cc597169db
Davide <darth.loru@gmai.com>
2023-09-01 19:08:33 +0200

    Fix database migration

--- gogas-persistence/src/main/resources/db/migration/V20230823_001__materialize_computed_orders_accounting.sql
+++ gogas-persistence/src/main/resources/db/migration/V20230823_001__materialize_computed_orders_accounting.sql
@@ -1,3 +1,4 @@
+-- Creating new table to store summary of user orders (total, count)
 CREATE TABLE userOrderSummary (
   userId uniqueidentifier NOT NULL,
   friendReferralId  uniqueidentifier,
@@ -11,6 +12,17 @@ CREATE TABLE userOrderSummary (
   CONSTRAINT FK_userOrderSummary_utenti FOREIGN KEY (userId) REFERENCES utenti(idUtente)
 )
 GO
+
+-- fixing any missing friend referral info in orders
+UPDATE o
+SET o.idReferenteAmico = u.idReferente
+FROM ordini o
+INNER JOIN utenti u ON o.idUtente = u.idUtente
+WHERE u.ruolo = 'S' AND o.idReferenteAmico IS NULL;
+
+-- adding new accounting reason for automatic charge of orders
+INSERT INTO causale (codiceCausale, segno, descrizione, codiceContabile)
+VALUES ('ORDINE_CAL', '-', 'Addebito ordine (automatico)', NULL);
 
 -- insert order summary for not computed orders (from accounting entries)
 INSERT INTO userOrderSummary (userId, friendReferralId, orderId, totalAmount, itemsCount, accountedItemsCount, aggregated)
@@ -54,19 +66,23 @@ FROM dateOrdini d
 ) as tot ON d.idDateOrdini = tot.idDateOrdine
 WHERE t.totaleCalcolato = 1 AND t.riepilogo = 1 AND d.stato IN (1, 2);
 
+-- Removing not confirmed accounting entries, replaced by userOrderSummary
+DELETE FROM movimenti WHERE confermato = 0;
+
+-- Adding shipping costs to accounting entries (new way of manage charges for users)
 UPDATE m
-SET m.importo = m.importo + t.importo
+SET m.importo = m.importo + t.importo, causale = 'ORDINE_CAL'
 FROM movimenti m
 INNER JOIN speseTrasporto t ON m.idDateOrdini = t.idDateOrdini AND m.idUtente = t.idUtente
-WHERE m.idDateOrdini IS NOT NULL AND t.importo IS NOT NULL
+WHERE m.idDateOrdini IS NOT NULL AND t.importo IS NOT NULL;
 
 -- Insert accounting entries for USERS (with confermato=false) for computed orders to replace "schedacontabile" view
 INSERT INTO movimenti (idMovimento, idUtente, dataMovimento, causale, idReferente, descrizione, importo, confermato, idDateOrdini)
 SELECT newid() as idMovimento,
-       CASE WHEN o.idReferenteAmico IS NOT NULL THEN o.idReferenteAmico ELSE o.idUtente END AS idUtente,
+       o.idUtente,
        d.dataConsegna AS dataMovimento,
        'ORDINE_CAL' as causale,
-       null AS idReferente,
+       o.idReferenteAmico AS idReferente,
        'Totale ordine ' + t.tipoOrdine + ' in consegna ' + CONVERT(VARCHAR, d.dataConsegna, 3) AS descrizione,
        ROUND(SUM(CASE WHEN [totaleCalcolato] = 1 THEN [qtaRitirataKg] * [prezzoKg] ELSE o.[prezzoKg] END), 2) + COALESCE(s.importo, 0) AS importo,
        0 AS confermato,
@@ -77,7 +93,8 @@ FROM dateOrdini d
     INNER JOIN utenti u ON o.idUtente = u.idUtente
     LEFT OUTER JOIN speseTrasporto s ON d.idDateOrdini = s.idDateOrdini AND s.idUtente = o.idUtente
 WHERE o.riepilogoUtente = 1 and o.contabilizzato = 1
-GROUP BY CASE WHEN o.idReferenteAmico IS NOT NULL THEN o.idReferenteAmico ELSE o.idUtente END,
+GROUP BY o.idUtente,
+         o.idReferenteAmico,
          d.dataConsegna,
          d.idDateOrdini,
          s.importo,
@@ -99,25 +116,64 @@ FROM dateOrdini d
      INNER JOIN ordini o ON d.idDateOrdini = o.idDateOrdine
      INNER JOIN utenti u ON o.idUtente = u.idUtente
      LEFT OUTER JOIN speseTrasporto s ON d.idDateOrdini = s.idDateOrdini AND s.idUtente = o.idUtente
-WHERE o.idReferenteAmico IS NOT NULL AND o.contabilizzato = 1
+WHERE o.idReferenteAmico IS NOT NULL AND o.riepilogoUtente = 0 AND o.contabilizzato = 1
 GROUP BY o.idUtente,
          o.idReferenteAmico,
          d.dataConsegna,
          d.idDateOrdini,
          s.importo,
          'Totale ordine ' + t.tipoOrdine + ' in consegna ' + CONVERT(VARCHAR, d.dataConsegna, 3)
+GO
 
--- Query to check if accounting entries are the same as "schedacontabile"
--- SELECT m.idUtente, dataMovimento as data,
--- CASE c.codiceCausale WHEN 'ORDINE_CAL' THEN m.descrizione ELSE c.descrizione + ' - ' + m.descrizione END as descrizione, c.segno,
--- CAST(m.importo AS DECIMAL(18, 5)) as importo,
--- CASE c.codiceCausale WHEN 'ORDINE_CAL' THEN COALESCE(m.idDateOrdini, idMovimento) ELSE idMovimento END as idRiga,
--- m.idDateOrdini
--- FROM movimenti m
--- INNER JOIN causale c ON m.causale = c.codiceCausale
--- WHERE m.idUtente IN (SELECT idUtente FROM utenti WHERE utente = <username>)
--- order by dataMovimento desc, CASE c.codiceCausale WHEN 'ORDINE_CAL' THEN m.descrizione ELSE c.descrizione + ' - ' + m.descrizione END
+-- View used to check if list of accounting entries matches with old "SchedaContabile"
+CREATE VIEW checkSchedaContabile AS
+SELECT idUtente, idReferente, dataMovimento as data,
+CASE WHEN c.codiceCausale = 'ORDINE_CAL' AND t.totaleCalcolato = 1 THEN m.descrizione ELSE c.descrizione + ' - ' + m.descrizione END as descrizione, c.segno,
+CAST(m.importo AS DECIMAL(18, 5)) as importo,
+CASE WHEN c.codiceCausale  = 'ORDINE_CAL' AND t.totaleCalcolato = 1 THEN COALESCE(m.idDateOrdini, idMovimento) ELSE idMovimento END as idRiga,
+m.idDateOrdini,
+riepilogo
+FROM movimenti m
+LEFT OUTER JOIN (SELECT d.idDateOrdini, t.totaleCalcolato, t.riepilogo FROM dateOrdini d INNER JOIN tipologiaOrdine t ON d.idTipologiaOrdine = t.idTipologiaOrdine) t ON t.idDateOrdini = m.idDateOrdini
+INNER JOIN causale c ON m.causale = c.codiceCausale
+GO
 
+-- Adjusting SchedaContabile nested views to exclude shipping costs (already included now in accounting entries)
+ALTER VIEW riepilogoMovimenti AS
+SELECT 'S' AS TipoMovimento, dbo.movimenti.idUtente, dbo.movimenti.dataMovimento AS data,
+dbo.causale.descrizione + ' - ' + dbo.movimenti.descrizione AS descrizione,
+dbo.causale.segno, dbo.movimenti.importo,
+dbo.movimenti.idMovimento AS idRiga, dbo.movimenti.idDateOrdini AS idOrdine
+FROM  dbo.movimenti
+INNER JOIN dbo.causale ON dbo.causale.codiceCausale = dbo.movimenti.causale
+WHERE confermato = 1
+GO
 
+ALTER VIEW riepilogoMovimentiAmici
+AS
+SELECT 'A' AS TipoMovimento, movimenti.idReferente, movimenti.dataMovimento AS data, causale.descrizione + ' - (' + utenti.nome + ' ' + utenti.cognome + ') ' + movimenti.descrizione AS descrizione, causale.segno, movimenti.importo, movimenti.idMovimento AS idRiga, movimenti.idDateOrdini AS idOrdine
+FROM causale INNER JOIN movimenti ON causale.codiceCausale = movimenti.causale
+             INNER JOIN utenti ON movimenti.idUtente = utenti.idUtente
+WHERE movimenti.idReferente IS NOT NULL AND confermato = 1
+GO
 
-
+-- QUERIES TO VERIFY ALIGNMENT OF SCHEDA CONTABILE
+--
+--select idUtente, FORMAT(data, 'yyyy-MM-dd') as data, descrizione, segno, FORMAT(SUM(importo), 'N2', 'it-it') as importo, idRiga, idDateOrdini FROM
+--(
+--  SELECT
+--  CASE WHEN riepilogo = 0 THEN COALESCE(idReferente, idUtente) ELSE idUtente END as idUtente,
+--  data, descrizione, segno, importo as importo, idRiga, idDateOrdini
+--  from checkSchedaContabile
+--  UNION ALL
+--  select idUtente, data, descrizione, segno, importo, idRiga, idDateOrdini
+--  from checkSchedaContabile
+--  where idReferente IS NOT NULL
+--) x
+--group by idUtente, data, descrizione, segno, idRiga, idDateOrdini
+--order by idUtente, data desc, idRiga, descrizione
+--
+--select idUtente, FORMAT(data, 'yyyy-MM-dd') as data, descrizione, segno, FORMAT(SUM(importo), 'N2', 'it-it') as importo, idRiga, idDateOrdini
+--from schedacontabile
+--group by idUtente, data, descrizione, segno, idRiga, idDateOrdini
+--order by idUtente, data desc, idRiga, descrizione
