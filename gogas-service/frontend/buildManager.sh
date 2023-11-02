0fd9632e90056e3d2688eddaafffa140cee2ac7c
edpsce <ermanno.scanagatta@eoc.ch>
2023-11-02 21:13:30 +0100

    Fixed built files copy

--- gogas-service/frontend/buildManager.sh
+++ gogas-service/frontend/buildManager.sh
@@ -3,5 +3,5 @@ rm -rf ../src/main/resources/templates/*
 rm -rf ../src/main/resources/static/*
 mkdir -p ../src/main/resources/templates
 mkdir -p ../src/main/resources/static
-mv ./build/index.html ../src/main/resources/templates/singlepage.html
-mv ./build/* ../src/main/resources/static/
+mv ./dist/index.html ../src/main/resources/templates/singlepage.html
+mv ./dist/* ../src/main/resources/static/
