b5a2ffbc84f35e454944db4d046c2fe3b2e29e70
Davide <darth.loru@gmai.com>
2023-12-13 23:38:54 +0100

    GOG-35 - Create skeleton to host legacy UI pages (missing static files)

--- gogas-service/frontend/buildManager.sh
+++ gogas-service/frontend/buildManager.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm -rf ../src/main/resources/templates/*
-rm -rf ../src/main/resources/static/*
-mkdir -p ../src/main/resources/templates
-mkdir -p ../src/main/resources/static
+#rm -rf ../src/main/resources/templates/*
+#rm -rf ../src/main/resources/static/*
+#mkdir -p ../src/main/resources/templates
+#mkdir -p ../src/main/resources/static
+rm -f ../src/main/resources/templates/singlepage.html
 mv ./dist/index.html ../src/main/resources/templates/singlepage.html
 mv ./dist/* ../src/main/resources/static/
