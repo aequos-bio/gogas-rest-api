c038f6e4186885ada0a679440dc475372f560b6d
Davide <darth.loru@gmai.com>
2024-01-12 18:17:00 +0100

    Sort in delivery orders from older to recent (delivey date ASC)

--- gogas-persistence/src/main/java/eu/aequos/gogas/persistence/repository/OrderRepo.java
+++ gogas-persistence/src/main/java/eu/aequos/gogas/persistence/repository/OrderRepo.java
@@ -110,7 +110,7 @@ public interface OrderRepo extends CrudRepository<Order, String>, JpaSpecificati
     @Query("SELECT o FROM Order o JOIN FETCH o.orderType t " +
             "WHERE function('DateAdd', hh, o.dueHour, o.dueDate) < CURRENT_TIMESTAMP AND o.deliveryDate >= function('convert', date, CURRENT_TIMESTAMP) " +
             "AND (t.external = true OR EXISTS (SELECT i.id FROM OrderItem i WHERE i.user = ?1 AND i.order = o.id)) " +
-            "ORDER BY o.deliveryDate DESC, t.description ASC")
+            "ORDER BY o.deliveryDate ASC, t.description ASC")
     List<Order> getInDeliveryOrders(String userId, Pageable pageable);
 
     @Query(value = "SELECT d.idDateOrdini AS orderId, o.idUtente as userId, " +
